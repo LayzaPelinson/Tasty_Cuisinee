1ff5ee8a0eec9a1383c9a0963c592acdf5ab8091
KAIO-BASTOS-ALVES <rm95196@estudante.fieb.edu.br>
2025-10-22 10:55:54 -0300

    asdsa

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   const navigate = (path: string, requireAuth = true) => {
     if (requireAuth && !isLogged) {
       alert("Você precisa estar logado para acessar a página")
-      window.location.href = "/login"
+      window.location.href = "/loginnew"
     } else {
       window.location.href = path
     }
